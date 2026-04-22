44686652a3675fff1ebc3f6460c2d74ec5e96565
Kamil Pasierski <pasierskidev@gmail.com>
2026-04-22 18:41:42 +0200

    add secure authentication flow and  dashboard view

--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
-# Create your views here.
+@login_required
+@never_cache
+def dashboard(request):
+    return render(request, 'analysis/dashboard.html', {'username': request.user.username})
