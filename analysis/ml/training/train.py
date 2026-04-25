c09d1c065db7458fbddcf9235df3462204333c1e
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-04-25 18:46:25 +0200

    update

--- analysis/ml/training/train.py
+++ analysis/ml/training/train.py
@@ -1,5 +1,6 @@
 from ml.models.cnn_model import build_model
 from ml.utils.dataset import load_datasets, prepare
+import tensorflow as tf
 
 DATA_DIR = "data/train"
 MODEL_PATH = "ml_models/model.h5"
@@ -15,10 +16,22 @@ def train():
     model.fit(
         train_ds,
         validation_data=val_ds,
-        epochs=20
+        epochs=20,
+        callbacks=[
+            tf.keras.callbacks.EarlyStopping(
+                monitor='val_loss',
+                patience=3,
+                restore_best_weights=True
+            ),
+            tf.keras.callbacks.ModelCheckpoint(
+                filepath=MODEL_PATH,
+                monitor='val_loss',
+                save_best_only=True
+            )
+        ]
     )
 
-    model.save(MODEL_PATH)
+    # model.save(MODEL_PATH)
     print("Model zapisany!")
 
 if __name__ == "__main__":
--- analysis/ml/utils/dataset.py
+++ analysis/ml/utils/dataset.py
@@ -1,33 +1,45 @@
 import tensorflow as tf
+from tensorflow.keras.utils import image_dataset_from_directory
 
 IMG_SIZE = (128, 32)
 BATCH_SIZE = 32
 
-def load_datasets(data_dir):
+"""
+BASE_DIR = "data/augmented_hw_dataset"
+
+train_ds = image_dataset_from_directory(f"{BASE_DIR}/training")
+val_ds   = image_dataset_from_directory(f"{BASE_DIR}/validation")
+test_ds  = image_dataset_from_directory(f"{BASE_DIR}/testing")
+"""
+
+def load_datasets(base_dir):
     train_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="training",
-        seed=42,
+        f"{base_dir}/training",
         image_size=IMG_SIZE,
         batch_size=BATCH_SIZE,
         color_mode="grayscale"
     )
 
     val_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="validation",
-        seed=42,
+        f"{base_dir}/validation",
         image_size=IMG_SIZE,
         batch_size=BATCH_SIZE,
         color_mode="grayscale"
     )
 
-    return train_ds, val_ds
+    test_ds = tf.keras.utils.image_dataset_from_directory(
+        f"{base_dir}/testing",
+        image_size=IMG_SIZE,
+        batch_size=BATCH_SIZE,
+        color_mode="grayscale"
+    )
+
+    train_ds = train_ds.shuffle(1000)
+
+    return train_ds, val_ds, test_ds
 
 
 def prepare(ds):
     ds = ds.map(lambda x, y: (x / 255.0, y))
-    ds = ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+    ds = ds.cache().shuffle(1000).prefetch(tf.data.AUTOTUNE)
     return ds
